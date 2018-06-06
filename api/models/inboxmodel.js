94c929fb906dc2f91d018d8bc1e34da98282afa7
Kevin <kevin.mueller3011@gmx.de>
2018-06-06 15:01:16 +0200

    Inbox Route SendMessage

--- api/models/inboxmodel.js
+++ api/models/inboxmodel.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 
 const inboxSchema = mongoose.Schema({
     inboxid: mongoose.Schema.Types.ObjectId,
-    receiver: mongoose.Schema.Types.ObjectId,
-    sender: mongoose.Schema.Types.ObjectId,
-    message: {Type: String, timesended: { type: Date, default: Date.now }}
+    userid: mongoose.Schema.Types.ObjectId,
+    messages: {type: Array, timesended: { type: Date, default: Date.now }, senderid: {type: mongoose.Schema.Types.ObjectId}, messagetext: {type: String}}
     
 });
 
--- api/routes/inboxroutes.js
+++ api/routes/inboxroutes.js
@@ -8,12 +8,13 @@ const router = express.Router();
 // Model Import
 const Inbox = require('../models/inboxmodel');
 
-
+// Creates new Inbox
 router.post('/create', (req, res, next) => {
     
     const newInbox = new Inbox({
         inboxid: new mongoose.Types.ObjectId(),
-    })
+        userid: req.body.userid
+    });
     
     newInbox.save()
     .then(result =>{
@@ -26,7 +27,30 @@ router.post('/create', (req, res, next) => {
     })
 
     
+});
+
+router.patch('/sendmessage', (req, res, next) => {
+    
+    const newMessage = {
+        senderid: req.body.senderid,
+        messagetext: req.body.messagetext,
+    }
+    
+    
+    
+    Inbox.findOneAndUpdate({inboxid: req.body.inboxid}, {$push: {messages: newMessage}})
+    .then(result => {
+        res.status(200).json(result);
+        console.log('message was send succesfully')
+    })
+    .catch(err => {
+        res.status(500).json(err);
+        console.log('message could not be send')
+    })
 })
+
+
+
 
 
 module.exports = router;
